ec06d15abd944e6435074335acd22e6b21c2412e
saurabhchavhan <saurabhchavhan12@gmail.com>
2020-08-31 16:16:57 +0530

    first commit

---
+++ aws-s3.sh
@@ -0,0 +1,14 @@
+#/bin/bash
+DATE=$(date %H-%M-%S)
+BACKUP=db-$DATE.sql
+DB_HOST=$1
+DB_PASSWORD=$2
+DB_NAME=$3
+AWS_SECRET=$4
+BUCKET_NAME=$5
+
+mysqldump -u root -h $DB_HOST -p$DB_PASSWORD $DB_NAME >tmp/$BACKUP && \
+export AWS_ACCESS_KEY_ID=AKIAX6V3WIYCVG4G3ING && \
+export AWS_SECRET_ACCESS_KEY=$AWS_SECRET && \
+echo "uploading your db backup"  && \
+aws s3 cp /tmp/$BACKUP s3://$BUCKET_NAME/$BACKUP
